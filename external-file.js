67080c25b4a4a2ea67a3ac97dbee6597e27cba36
moaaz90 <moaaz.yahia.zakaria@gmail.com>
2023-04-07 07:42:47 +0300

    first commit

---
+++ external-file.js
@@ -0,0 +1,10 @@
+// var x = 10;
+// var y = 20;
+// var z = 100;
+// var Name = Moaaz;
+// var firstName = yahia;
+// var lastName = zakaria;
+document.getElementById("text").innerHTML = moaaz;
+
+
+confirm("start");
